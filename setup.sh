00203e6f30f42f3c0a6b368983e5e4d481d2b940
wyde <wyde@csie.ntu.edu.tw>
2017-11-03 15:08:03 +0800

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/bash
 
-echo " update .bash_profile"
-if [ -f ~/dotfiles/.bash_profile ]; then
-    cp ~/dotfiles/.bash_profile ~/.bash_profile
-fi
-
 echo " update .bashrc"
 if [ -f ~/dotfiles/.bashrc ]; then
     cp ~/dotfiles/.bashrc ~/.bashrc
